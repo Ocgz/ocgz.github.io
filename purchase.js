0b0473d47d0716e826e810bc60f5f60f982d5fff
Ocgz <94432485+Ocgz@users.noreply.github.com>
2021-11-24 18:02:55 -0800

    Add files via upload

---
+++ purchase.js
@@ -0,0 +1,31 @@
+function myFunction()
+{   // get the elements in the input field
+    var tickets = document.getElementById( "ticket_amount" );
+
+
+     // turn into integers
+     var value1 = parseInt( tickets.value );
+
+    //get result
+    var result = document.getElementById( "result" );
+
+
+    // calculations
+    var total = value1 * 100;
+    var tax = total * 0.09;
+    var taxx = tax.toFixed(2);
+    var total2 = total+ tax;
+
+    if ( value1 >= 1 )
+    {
+        result.innerHTML = "<h4>Your total + tax for this purchase is: $" + total2 + " and the tax is: $" + taxx + "</h4>";
+    } // end if
+} // end myFunction
+
+function start()
+{
+    var Button = document.getElementById( "theButton" );
+    Button.addEventListener( "click", myFunction, false );
+}
+
+window.addEventListener("load", start, false);
